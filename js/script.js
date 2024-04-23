79a890608b7444147c8c884781be67f617cbb82a
Ahmed Wael <wael92318@gmail.com>
2024-04-23 23:06:48 +0200

    Uploaded files

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon and navbar when click on any link
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// Remove (active) class form all and add to the pressed section only
+let links = document.querySelectorAll("header nav a");
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((link) => {
+      link.classList.remove("active");
+      e.target.classList.add("active");
+    });
+  });
+});
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container , .portfolio-box , .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img ", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content ", { origin: "right" });
+
+/*==================== typed js ====================*/
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Freelancer","Seeker"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+/*==================== Add Year Dynamicly ====================*/
+let date = document.querySelector(".date");
+let year = new Date().getFullYear();
+date.innerHTML = year;
